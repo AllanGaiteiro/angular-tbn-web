a2d8605731cce9173ea92735aa01eba00eff4afd
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2022-07-03 14:32:37 -0300

    crate coponent sidenav

---
+++ src/app/sidenav/sidenav.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+//import { MatSidenav } from '@angular/material/sidenav';
+// import * as HeaderComponent from '../components/header/header.component';
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.scss'],
+})
+export class SidenavComponent implements OnInit {
+  // @ViewChild('sidenav') public sidenav: MatSidenav;
+
+  public fontSizePx: number = 50;
+  public paragraph?: NodeListOf<HTMLParagraphElement>;
+  public reason: string = '';
+  public expandedMenu = false;
+  public shouldRun = true;
+  public panelOpenState = false;
+  constructor() {
+    // no-empty-function
+  }
+
+  public ngOnInit(): void {
+    this.setFontSize(this.fontSizePx);
+  }
+
+
+
+  public setFontSize = (value: number): void => {
+    this.paragraph = document.querySelectorAll('p');
+    console.log(value)
+    this.paragraph.forEach((p) => {
+      p.style.fontSize = `${value}px`;
+    });
+  };
+}

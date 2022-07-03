8cd5c40ceae879708d5f6f56798a2cc2e5f7fc24
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2022-07-03 14:33:12 -0300

    change app coponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   title = 'tbn-web';
-
-  ngOnInit() {
+  public reason: string = '';
+  public matIconMenu: 'keyboard_return' | 'keyboard_tab' = 'keyboard_return';
+  constructor() {
+    // empty
   }
+
+  public ngOnInit(): void {
+  }
+
 }
